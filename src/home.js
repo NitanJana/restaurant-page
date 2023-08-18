1704d4d6f83c90a10b30a013bc0f2ef3374faf7f
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-08-18 12:33:54 +0530

    feat: add home module to content dynamically

--- src/home.js
+++ src/home.js
@@ -1,24 +1,22 @@
 import chefImage from './chef.jpg';
 
 export default function home() {
-  const home = document.createElement('main');
-  const content = document.createElement('content');
+  const main = document.createElement('main');
   const contentPara1 = document.createElement('p');
   const chef = new Image();
   const contentPara2 = document.createElement('p');
 
   
-  home.classList.add('main');
-  content.classList.add('content');
+  main.classList.add('main');
   
   contentPara1.textContent = 'Best Pizza';
   chef.src = chefImage;
   contentPara2.textContent = 'Order Now!';
   
-  home.appendChild(content);
-  content.appendChild(contentPara1);
-  content.appendChild(chef);
-  content.appendChild(contentPara2);
+  
+  main.appendChild(contentPara1);
+  main.appendChild(chef);
+  main.appendChild(contentPara2);
 
-  return home;
+  return main;
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import './style.css';
 import header from './header.js';
 import home from './home.js';
+import menu from './menu.js';
+import about from './about.js';
 
 document.body.appendChild(header());
-document.body.appendChild(home());
+const content = document.createElement('div');
+content.classList.add('content');
+document.body.appendChild(content);
+
+content.appendChild(home());
 
 const tabList = Array.from(document.querySelectorAll('.tab'));
 tabList.forEach((tab) => {
   tab.addEventListener('click', (e) => {
+    if (e.target.textContent === 'Home') {
+      content.innerHTML = "";
+      content.appendChild(home());
+
+    } else if (e.target.textContent === 'Menu') {
+      content.innerHTML = "";
+      content.appendChild(menu());
+
+    } else if (e.target.textContent === 'About') {
+      content.innerHTML = "";
+      content.appendChild(about());
+
+    }home
+
     tabList.forEach((item) => {
       item.classList.remove('active-tab');
     });
